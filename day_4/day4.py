af0648bae2d172f8bfd277820e0ff06ad7661ec3
cnzh2020 <740720516@qq.com>
2021-07-19 22:18:47 +0800

    update day5

--- day_4/day4.py
+++ day_4/day4.py
@@ -1,6 +1,14 @@
 # Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
 
+T = 'Thirty'
+D = 'Days'
+O = 'Of'
+P = 'Python'
+Space = ' '
+print(T+Space+D+Space+O+Space+P)
+
 # Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
 
 # Declare a variable named company and assign it to an initial value "Coding For All".
 
@@ -57,26 +65,30 @@
 # '   Coding For All      '  , remove the left and right trailing spaces in the given string.
 
 # Which one of the following variables return True when we use the method isidentifier():
-    '''
+
+'''
     30DaysOfPython
     thirty_days_of_python
-    '''
+'''
 
 # The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
 
 # Use the new line escape sequence to separate the following sentences.
+
 '''
     I am enjoying this challenge.
     I just wonder what is next.
 '''
 
 # Use a tab escape sequence to write the following lines.
+
 '''
     Name      Age     Country
     Asabeneh  250     Finland
 '''
 
 # Use the string formating method to display the following:
+
 '''
 radius = 10
 area = 3.14 * radius ** 2
@@ -84,6 +96,7 @@ The area of a cricle with radius 10 is 314 meters square.
 '''
 
 # Make the following using string formating methods:
+
 '''
 8 + 6 = 14
 8 - 6 = 2
